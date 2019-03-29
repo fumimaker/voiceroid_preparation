4a74632d0d5abe80568526c10acd73bb2f871a92
fumi <fumi01200924@gmail.com>
2019-03-30 01:55:12 +0900

    句点句読点の区切りができるようになった。空リストの削除できるようになった。

--- text.py
+++ text.py
@@ -1,23 +1,65 @@
 import re
+
 # !/usr/bin/python
 # coding: UTF-8
-with open("./counter.txt", mode='r') as f:
-    fileCounter = int(f.read())
-    print("ファイル数は: " + str(fileCounter))
 
-f = open('./input.txt', encoding="utf-8")
-data1 = f.read()  # ファイル終端まで全て読んだデータを返す
-f.close()
+def flatten_with_any_depth(nested_list):
+    """深さ優先探索の要領で入れ子のリストをフラットにする関数"""
+    # フラットなリストとフリンジを用意
+    flat_list = []
+    fringe = [nested_list]
 
-lines1 = data1.split('\n') # 改行で区切る(改行文字そのものは戻り値のデータには含まれない)
+    while len(fringe) > 0:
+        node = fringe.pop(0)
+        # ノードがリストであれば子要素をフリンジに追加
+        # リストでなければそのままフラットリストに追加
+        if isinstance(node, list):
+            fringe = node + fringe
+        else:
+            flat_list.append(node)
 
-l = []
-for line in lines1:
-    l.append(re.findall(".*?。|.*$", line))
+    return flat_list
 
-T = []
-T = filter(lambda a: a != '', l)
-print(T)
+
+def trimingByMark(list, character):
+    l = []
+    for now_text in list:
+        l.append(re.findall(".*?" + character + "|.*$", now_text))
+
+    return flatten_with_any_depth(l)
+
+
+def main():
+    with open("./counter.txt", mode='r') as f:
+        fileCounter = int(f.read())
+        print("ファイル数は: " + str(fileCounter))
+
+    f = open('./input.txt', encoding="utf-8")
+    data1 = f.read()  # ファイル終端まで全て読んだデータを返す
+    f.close()
+
+    lines1 = data1.split('\n') # 改行で区切る(改行文字そのものは戻り値のデータには含まれない)
+    lines1 = trimingByMark(lines1, "、")
+    lines1 = trimingByMark(lines1, "。")
+    lines1 = trimingByMark(lines1, "！")
+    lines1 = trimingByMark(lines1, "？")
+    lines1 = [x for x in lines1 if x]
+
+
+    """
+    l = []
+    for line in lines1:
+        l.append(re.findall(".*?、|.*$", line))
+
+    l = flatten_with_any_depth(l)
+    tmp = []
+    for j in l:
+        tmp.append(re.findall(".*?、|.*$", j))
+    l = flatten_with_any_depth(tmp)
+
+    
+    """
+    print([x for x in lines1 if x])
 
 """
     if (i != '') or (i != ' '):
@@ -29,3 +71,5 @@ print(T)
 with open("./counter.txt", mode='w') as f:
     f.write(str(fileCounter))
 """
+if __name__ == '__main__':
+    main()

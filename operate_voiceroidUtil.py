ab4acde0ce1fa6723cae2d5847b1436c8422072b
fumi <fumi01200924@gmail.com>
2019-03-31 02:32:39 +0900

    Add text

--- operate_voiceroidUtil.py
+++ operate_voiceroidUtil.py
@@ -4,6 +4,8 @@ import os
 import re
 import sys
 import time
+import subprocess
+
 # coding: UTF-8
 
 
@@ -15,7 +17,6 @@ def checkPlay():
         except TypeError:
                 print("Type Error")
                 return True
-
 
 
 print('defaultencoding:', sys.getdefaultencoding())
@@ -32,7 +33,7 @@ word_list = []
 origin = 0
 for i in range(count):
         path = "./output/voice_" + str(i+origin) + ".txt"
-        with open(path,'r') as f:
+        with open(path,'r',encoding="utf-8") as f:
                 s = f.read()
                 print(s)
                 word_list.append(s)
@@ -55,3 +56,13 @@ for j in range(count):
         pyautogui.hotkey('f6')
         print("保存待機中...")
         time.sleep(2.5)
+        if j%100==0:
+                cmd = 'taskkill /im VoiceroidEditor.exe'
+                returncode = subprocess.call(cmd)
+                time.sleep(1)
+                pyautogui.typewrite("tab")
+                time.sleep(1)
+                pyautogui.typewrite("enter")
+                time.sleep(3)
+                subprocess.Popen("C:\Program Files (x86)\AHS\VOICEROID2\VoiceroidEditor.exe")
+                time.sleep(3)

32c41daf52456c51c33fec0fc567cee805818247
fumi <fumi01200924@gmail.com>
2019-03-30 23:36:08 +0900

    正しく画像を認識するように修正。ようやく意味のあるものになりました。

--- operate_voiceroidUtil.py
+++ operate_voiceroidUtil.py
@@ -5,18 +5,28 @@ import re
 import sys
 import time
 # coding: UTF-8
-util_x = 500
-util_y = 500
+
+
+def checkPlay():
+        try:
+                pos = pyautogui.locateCenterOnScreen('play.PNG', region=(0,0, 1000, 1000))
+                print("found")
+                return False
+        except TypeError:
+                print("Type Error")
+                return True
+
+
 
 print('defaultencoding:', sys.getdefaultencoding())
 
 files = os.listdir("./output")
 count = 0
 for file in files:
-    index = re.search(".txt", file)
-    if index:
-        count = count + 1
-print(count)
+        index = re.search(".txt", file)
+        if index:
+                count = count + 1
+print("変換ファイル数: "+str(count))
 
 word_list = []
 
@@ -28,26 +38,19 @@ for i in range(count):
                 word_list.append(s)
 print()
 
-
-img_play_x,img_play_y = pyautogui.locateCenterOnScreen('play.PNG',grayscale=True,region=(0,0, 1000, 1000))
-
 for j in range(count):
         print(j)
         flg = True
-        print("保存待機中...")
+        print("play検索")
         while flg:
-                if pyautogui.onScreen(img_play_x, img_play_y):
-                        flg = False
-                else:
-                        flg = True
-        time.sleep(0.5)
-        img_x,img_y = pyautogui.locateCenterOnScreen('inputArea.PNG',grayscale=True,region=(0,0, 1000, 1000))
-        pyautogui.moveTo(img_x+100, img_y+100, duration=0.1)
-        pyautogui.click(img_x+100, img_y+100)
-        #pyautogui.click(util_x, util_y)
+                flg = checkPlay()
+
+        pyautogui.click(500, 500)
         pyperclip.copy(word_list[j])
         pyautogui.hotkey('ctrl','v')
-        time.sleep(0.2)
+
+        time.sleep(0.1)
+
         pyautogui.hotkey('f6')
-        print(word_list[j])
-        time.sleep(0.5)
+        print("保存待機中...")
+        time.sleep(1.5)

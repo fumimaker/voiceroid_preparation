b4027f9bcb777b9b65420334be49e3633c76ed62
fumi <fumi01200924@gmail.com>
2019-03-30 21:51:58 +0900

    もっと良くなった
    VoiceroiUtilは画像処理でより高速に処理できるようになった。これから大規模テスト

--- operate_voiceroidUtil.py
+++ operate_voiceroidUtil.py
@@ -4,7 +4,7 @@ import os
 import re
 import sys
 import time
-
+# coding: UTF-8
 util_x = 500
 util_y = 500
 
@@ -29,12 +29,25 @@ for i in range(count):
 print()
 
 
+img_play_x,img_play_y = pyautogui.locateCenterOnScreen('play.PNG',grayscale=True,region=(0,0, 1000, 1000))
+
 for j in range(count):
         print(j)
-        pyautogui.click(util_x, util_y)
+        flg = True
+        print("保存待機中...")
+        while flg:
+                if pyautogui.onScreen(img_play_x, img_play_y):
+                        flg = False
+                else:
+                        flg = True
+        time.sleep(0.5)
+        img_x,img_y = pyautogui.locateCenterOnScreen('inputArea.PNG',grayscale=True,region=(0,0, 1000, 1000))
+        pyautogui.moveTo(img_x+100, img_y+100, duration=0.1)
+        pyautogui.click(img_x+100, img_y+100)
+        #pyautogui.click(util_x, util_y)
         pyperclip.copy(word_list[j])
         pyautogui.hotkey('ctrl','v')
-        #pyautogui.hotkey('ctrl','s')
-        img_x,img_y = pyautogui.locateCenterOnScreen('save.PNG')
+        time.sleep(0.2)
+        pyautogui.hotkey('f6')
         print(word_list[j])
-        time.sleep(3)
+        time.sleep(0.5)
